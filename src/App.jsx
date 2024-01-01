36bb101513438b33207cd7b892a115804bb18261
Anastasia Radchuk <nastasiyaradchuk@gmail.com>
2023-12-31 21:31:46 -0500

    added scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello World
-        </h1>
-    </div>
+    <Home />
   )
 }
 
